dd13e869020fde86a692ec696a2341dc448e9b00
KuroXSub <qurrotaayunliulinuha@gmail.com>
2025-04-17 09:58:00 +0700

    Remove Laravel Component

--- app/Livewire/Auth/ResetPassword.php
+++ app/Livewire/Auth/ResetPassword.php
@@ -12,7 +12,7 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Locked;
 use Livewire\Component;
 
-#[Layout('components.layouts.auth')]
+#[Layout('layouts.clean')]
 class ResetPassword extends Component
 {
     #[Locked]
